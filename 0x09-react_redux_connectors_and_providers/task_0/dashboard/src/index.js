85c7281e5c9f9c272be19d660a5fcf2801993fc3
NEAZYIT <touihnazih28@gmail.com>
2024-05-15 17:58:12 +0100

    feat: Implement Redux store and provider in index.js
    This commit implements a Redux store and provider in the index.js file.
    
    The createStore function from Redux is used to create a store that contains the uiReducer state.
    
    A provider is then implemented using the Provider component from React-Redux. The store is passed to the provider, which makes it available to all components within the application.
    
    This commit is a significant step forward in the development of this project, as it allows the application to manage its state using Redux.

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,17 +1,18 @@
+// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import uiReducer from './reducers/uiReducer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// Step 1: Create the Redux store
+const store = createStore(uiReducer);
+
+// Step 2: Use the Provider to pass the store to the App
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
