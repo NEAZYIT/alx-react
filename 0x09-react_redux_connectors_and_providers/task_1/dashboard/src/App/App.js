ba9b907381975efb57bc7bf32ca3f808c0c56d0a
NEAZYIT <touihnazih28@gmail.com>
2024-05-15 18:26:01 +0100

    feat: Implement Redux state mapping and action dispatching in App component
    This commit implements Redux state mapping and action dispatching in the App component using the connect function from React-Redux.
    
    The mapStateToProps function is created to map the isLoggedIn and displayDrawer properties from the Redux state to the component's props.
    
    The mapDispatchToProps object is created to map the displayNotificationDrawer and hideNotificationDrawer action creators to the component's props.
    
    This mapping allows the App component to access the isLoggedIn and displayDrawer state from the Redux store, and to dispatch the displayNotificationDrawer and hideNotificationDrawer actions.
    
    This commit is a significant step forward in the development of this project, as it allows the App component to leverage the benefits of Redux state management and action dispatching.

--- 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.js
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { displayNotificationDrawer, hideNotificationDrawer } from '../actions/uiActionCreators';
 
 /**
  * Maps the Redux state to the props of the component.
+ * This function is used to connect the Redux state to the React component.
+ * It takes the Redux state as an argument and returns an object containing the props derived from the state.
  *
  * @param {Object} state - The Redux state object.
  * @returns {Object} - An object containing the props derived from the state.
@@ -15,25 +18,44 @@ const mapStateToProps = (state) => {
 };
 
 /**
+ * Maps the action creators to the props of the component.
+ * This object is used to connect the action creators to the React component.
+ * It contains the functions to display and hide the notification drawer.
+ */
+const mapDispatchToProps = {
+    displayNotificationDrawer,
+    hideNotificationDrawer
+};
+
+/**
  * Represents the main App component.
  */
 class App extends Component {
     /**
      * Render method of the App component.
+     * This method is responsible for rendering the JSX of the component.
      * 
      * @returns {JSX.Element} - The JSX for the component.
      */
     render() {
-        const { isLoggedIn, displayDrawer } = this.props;
+        const { isLoggedIn, displayDrawer, displayNotificationDrawer, hideNotificationDrawer } = this.props;
 
         return (
             <div>
                 {/* Your component JSX here */}
                 {isLoggedIn && <p>User is logged in</p>}
                 {displayDrawer && <p>Notification Drawer is visible</p>}
+
+                {/* Example buttons to trigger the actions */}
+                <button onClick={displayNotificationDrawer}>Show Notification Drawer</button>
+                <button onClick={hideNotificationDrawer}>Hide Notification Drawer</button>
             </div>
         );
     }
 }
 
-export default connect(mapStateToProps)(App);
+/**
+ * Connects the Redux state and action creators to the App component.
+ * This is a higher-order component provided by React Redux that connects a React component to a Redux store.
+ */
+export default connect(mapStateToProps, mapDispatchToProps)(App);
