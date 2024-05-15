15ee18922bb095e67d4529e298fcb25f95a14ff3
NEAZYIT <touihnazih28@gmail.com>
2024-05-15 19:39:21 +0100

    This commit adds the following features:
    Creates a Redux store with the root reducer and thunk middleware.
    Adds extension support for the Redux DevTools.
    Wraps the App component with the Provider component from react-redux, making the Redux store available to all components in the application.
    This commit improves the application by providing a centralized state management solution with Redux. This allows for better organization and maintainability of the application's state.

--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -1,35 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';  // Import compose from Redux
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers/rootReducer'; // Import the root reducer
+import rootReducer from './reducers';
 import App from './App/App';
 import './index.css';
 
 /**
  * Creates a Redux store with the root reducer, thunk middleware, and extension support.
- * It uses the createStore function from Redux to create the store.
- * The root reducer is passed as the first argument to define the initial state and handle state updates.
- * Thunk middleware is applied using applyMiddleware to enable asynchronous actions.
- * The Redux DevTools extension is enabled using the composeEnhancers function, which allows for debugging and time-travel functionality in the browser.
  */
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;  // Add extension support using composeEnhancers
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk))  // Pass the thunk middleware inside composeEnhancers
 );
 
 /**
  * Renders the React application with the Redux store provider.
- * The ReactDOM.render function is used to render the React application.
- * The Provider component from react-redux is used to wrap the App component and provide the Redux store to all the components in the application.
- * The store variable, which holds the created Redux store, is passed as a prop to the Provider component.
- * The document.getElementById('root') selects the DOM element with the ID 'root' and mounts the rendered React application there.
+ * The App component is wrapped with the Provider component from react-redux,
+ * which makes the Redux store available to all the components in the application.
  */
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-)
+);
